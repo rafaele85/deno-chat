302a8779304118cf49876b4c15df54856fe53ee3
Rafael E <raf65728@gmail.com>
2022-07-10 10:02:21 +0300

    added API for messages

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts'
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+
+const messages = []
+
+const router = new Router()
+
+router
+    .get('/', (context) => {
+        context.response.body = 'Chat server!'
+    })
+    .get('/messages', (context) => {
+        context.response.body = messages
+    })
+    .post('/', async (context) => {
+        const message = await context.request.body().value
+        messages.push(message)
+        context.response.body = messages
+    })
 
 const app = new Application();
 
-app.use((ctx) => {
-    ctx.response.body = "Hello World 3!";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
