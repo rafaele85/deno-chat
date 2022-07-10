437b4d542a62cef5994dfeef88cda525eba662a9
Rafael E <raf65728@gmail.com>
2022-07-10 16:22:44 +0300

    move messages retrieval to island add cors handling

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -20,8 +20,8 @@ router
 
 const app = new Application();
 
-app.use(router.routes());
 app.use(oakCors())
+app.use(router.routes());
 app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
