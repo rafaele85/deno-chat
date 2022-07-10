3e6e86383bfd8bee6627e300b07f75538b7e335e
Rafael E <raf65728@gmail.com>
2022-07-10 09:45:20 +0300

    some changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response('Hello world', {
+  const response = new Response('Hello world 2!', {
       headers: {'content-type': 'text/plain'}
   })
 
